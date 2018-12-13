4ed63a75de4984e63fd49d650ab9f69b2d46f326
chenhr <chenghr@qianqiusoft.com>
2018-12-14 01:00:55 +0800

    左侧菜单

--- src/admin.js
+++ src/admin.js
@@ -3,12 +3,11 @@ import { Row,Col } from 'antd';
 import Header from './components/Header/index'
 import Footer from './components/Footer/index'
 import NavLeft from './components/NavLeft/index'
-import './css/index.css'
+import './css/common.css'
 export default class Admin extends React.Component{  //01.继承才能实现生命周期
 
     render(){
         return ( //02.必须只有一个根节点
-        
             <Row className="container">
                 <Col span="3" className="nav-left"> 
                    <NavLeft/>
--- src/components/NavLeft/index.js
+++ src/components/NavLeft/index.js
@@ -1,11 +1,51 @@
 import React from 'react'
-
+import MenuConfig from './../../config/menuConfig'
+import './index.css'
+import { Menu} from 'antd';
+const SubMenu = Menu.SubMenu;
 export default class NavLeft extends React.Component{
+    componentWillMount(){
+        const menuTreeNode=this.renderMenu(MenuConfig)
 
+        this.setState({  //02.获取数据之后，通过 this.setState()方法把数据绑定进去
+                         //调用 this.setState之后就会调用 render()渲染数据 
+            menuTreeNode
+        })
+    }
+    //01.使用递归得定义一个函数，用于菜单熏染
+    /** 
+    renderMenu(){  //事件绑定不适合这个方式，this操作不方便
+    }*/
+    renderMenu=(data)=>{   
+          console.log(data)
+          return data.map((item)=>{  //使用map遍历
+             if(item.children){ //有children子节点需要再一次遍历
+                 return (
+                     <SubMenu title={item.title} key={item.key}>
+                        {this.renderMenu(item.children)}                            
+                     </SubMenu>
+                 )
+             }   
+             return <Menu.Item title={item.title} key={item.key}>{item.title}</Menu.Item>
+          })     
+    }
     render(){
         return(
-            <div style={{backgroundColor:'red'}}>
-                NavLeft
+            <div>
+                <div className="logo">
+                   <img src="/assets/logo-ant.svg" alt="首页"></img>
+                   <h1>frontsys</h1> 
+                </div>
+                <Menu theme="dark">
+                {/* <SubMenu key="sub1" title={<span><Icon type="mail" /><span>Navigation One</span></span>}>
+                    <Menu.Item key="1">Option 1</Menu.Item>
+                    <Menu.Item key="2">Option 2</Menu.Item>
+                    <Menu.Item key="3">Option 3</Menu.Item>
+                    <Menu.Item key="4">Option 4</Menu.Item>
+                </SubMenu> */}
+                 {/* {this.setState.menuTreeNode} */}
+                 { this.state.menuTreeNode }
+                </Menu>
             </div>
         )
     }
