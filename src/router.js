e39bcd642ae09e0019fb6ccc030f8d4b840475c6
chenhr <chenghr@qianqiusoft.com>
2018-12-21 08:48:13 +0800

    路由和ui完成

--- src/router.js
+++ src/router.js
@@ -2,9 +2,21 @@ import React from 'react'
 import { HashRouter, Route, Link, Switch} from 'react-router-dom'
 import App from './App.js'
 import Login from './pages/login/login.js'
-import Admin from './admin' 
-import Buttons from './pages/ui/buttons/index.js'
+import Admin from './admin'
+import NoMatch from './pages/nomatch/index.js';
+import Buttons from './pages/ui/buttons.js'
+import Modals from './pages/ui/modals.js'
+import Loadings from './pages/ui/loadings'
+import Notice from './pages/ui/notice'
+import Messages from './pages/ui/messages'
+import Tabs from './pages/ui/tabs'
+import Gallery from './pages/ui/gallery'
+import Carousel from './pages/ui/carousel'
 
+import FormLogin from './pages/form/login'
+import FormRegister from './pages/form/register'
+
+import BasicTable from './pages/table/basicTable'
 export default class IRouter extends React.Component{
 
     render(){
@@ -15,8 +27,25 @@ export default class IRouter extends React.Component{
                    <Route path='/login' component={Login}></Route>
                    {/* render={()=>} 箭头函数加{}自动返回,不用加return */}
                    <Route path='/' render={()=>    //注意 父路由得path='/' 是匹配规则，不能设置错， // 设置为path='/admin'否则不能匹配
-                       <Admin>                   
-                        <Route path="/ui/buttons" component={Buttons} />
+                       <Admin>
+                           {/* <Switch>设置单个匹配，否则会匹配两个路由，页面会加载两个页面 */}
+                           <Switch>
+                            <Route path="/ui/buttons" component={Buttons} />
+                            <Route path="/ui/modals" component={Modals} />
+                            <Route path="/ui/loadings" component={Loadings} />
+                            <Route path="/ui/notification" component={Notice} />
+                            <Route path="/ui/notification" component={Notice} />
+                            <Route path="/ui/messages" component={Messages} />
+                            <Route path="/ui/tabs" component={Tabs} />
+                            <Route path="/ui/gallery" component={Gallery} />
+                            <Route path="/ui/carousel" component={Carousel} />
+
+                            <Route path="/form/login" component={FormLogin} />
+                            <Route path="/form/reg" component={FormRegister} />
+
+                            <Route path="/table/basic" component={BasicTable} />
+                            <Route component={NoMatch} />
+                           </Switch>                   
                        </Admin>
                     }/>
                    <Route path='/order/detail' component={Login}></Route>
